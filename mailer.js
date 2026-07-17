bda79da43cf9f9b7057fdd2ed018ae17e3c6c244
asaipraneethgoud04-cmd <asaipraneethgoud04@gmail.com>
2026-07-17 16:47:44 +0530

    mailer

--- mailer.js
+++ mailer.js
@@ -6,8 +6,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: process.env.SMTP_PORT || 587,
-  secure: false,
+  port: Number(process.env.SMTP_PORT) || 465,
+  secure: Number(process.env.SMTP_PORT) === 465 || !process.env.SMTP_PORT, // true for 465, false for 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
